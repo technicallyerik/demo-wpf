d1882ca62284d02eae1e5cac57fa21c0b00fad7c
Erik Weiss <eweiss@nerdery.com>
2013-08-08 13:37:36 -0500

    Added a couple of examples of ways to asynchronously call functions.

--- Presenter/MainWindowPresenter.cs
+++ Presenter/MainWindowPresenter.cs
@@ -4,11 +4,17 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Threading;
 using CurrencyConversion.Models;
 using CurrencyConversion.Services;
+using System.Threading;
+using System.Runtime.Remoting.Messaging;
+using System.Windows;
 
 namespace CurrencyConversion.Presenter
 {
+    public delegate Dictionary<string, string> AsyncCurrencyCaller();
+
     public class MainWindowPresenter
     {
         private IMainWindow View { get; set; }
@@ -29,22 +35,55 @@ namespace CurrencyConversion.Presenter
             GetCurrencies();
         }
 
+
+        /// <summary>
+        /// Gets the currencies.  This example uses BeginInvoke with a callback to hit the service asynchronously. 
+        /// </summary>
         private void GetCurrencies()
         {
-            Model.AvailableCurrencyCodes = new ObservableCollection<CurrencyModel>();
-            Dictionary<string, string> currencies = CurrencyService.GetCurrencies();
-            foreach (KeyValuePair<string, string> currency in currencies)
-            {
-                Model.AvailableCurrencyCodes.Add(new CurrencyModel { Code = currency.Key, Name = currency.Value});
-            }
+            // Create and initiate the async call delegate
+            var caller = new AsyncCurrencyCaller(CurrencyService.GetCurrencies);
+            caller.BeginInvoke(GetCurrenciesCallback, null);
         }
 
-        public void Calculate()
+        private void GetCurrenciesCallback(IAsyncResult result)
         {
+            // Retrieve the delegate and results
+            AsyncResult asyncResult = (AsyncResult) result;
+            AsyncCurrencyCaller caller = (AsyncCurrencyCaller) asyncResult.AsyncDelegate;
+            Dictionary<string, string> currencies = caller.EndInvoke(result);
+
+            // GetCurrenciesCallback will be called in a different thread than the UI, 
+            // so we need to use the dispatcher to update the dependency properties.
+            Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Background, (Action)(() =>
+            {
+                Model.AvailableCurrencyCodes = new ObservableCollection<CurrencyModel>();
+                foreach (KeyValuePair<string, string> currency in currencies)
+                {
+                    Model.AvailableCurrencyCodes.Add(new CurrencyModel { Code = currency.Key, Name = currency.Value });
+                }
+            }));
+        }
+
+
+        /// <summary>
+        /// Calculates this exchange rate value.  This example uses async await, new to C# 5
+        /// </summary>
+        public async void Calculate()
+        {
+            // We need to retrieve a copy of these variables locally 
+            // because they can't be accessed within the async task
+            string sourceCurrencyCode = Model.SourceCurrencyCode;
+            string destinationCurrencyCode = Model.DestinationCurrencyCode;
+
             decimal exchangeRate;
             try
             {
-                exchangeRate = CurrencyService.GetExchangeRate(Model.SourceCurrencyCode, Model.DestinationCurrencyCode);
+                // Create an async task and execute it
+                Task<decimal> getExchangeRateTask = Task<decimal>.Factory.StartNew(() => CurrencyService.GetExchangeRate(sourceCurrencyCode, destinationCurrencyCode));
+
+                // Retrieve the value once the async task is done
+                exchangeRate = await getExchangeRateTask;
             }
             catch (CurrencyServiceException ex)
             {
